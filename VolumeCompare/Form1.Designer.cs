56a15f36f9318d01b893881b37ef086793dd76ed
Jacek Jabłoński <jackeneczek@gmail.com>
2017-01-03 18:31:25 +0100

    changed the way of scanning subfolders; fixed an issue with insuficient priviledges with accessing some crucial system program files; rebuilt the GUI

--- VolumeCompare/Form1.Designer.cs
+++ VolumeCompare/Form1.Designer.cs
@@ -13,6 +13,7 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
+            this.backgroundWorker1.CancelAsync();
             if (disposing && (components != null))
             {
                 components.Dispose();
@@ -40,14 +41,23 @@
             this.button4 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.listBox3 = new System.Windows.Forms.ListBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -75,6 +85,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(53, 6);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(537, 20);
             this.textBox1.TabIndex = 2;
             // 
@@ -82,6 +93,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(53, 32);
             this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(537, 20);
             this.textBox2.TabIndex = 3;
             // 
@@ -93,7 +105,7 @@
             this.button3.TabIndex = 4;
             this.button3.Text = "Porównaj z użyciem zewnętrznego programu Md5";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Visible = false;
             // 
             // listBox1
             // 
@@ -112,23 +124,26 @@
             this.button4.TabIndex = 6;
             this.button4.Text = "Porównaj wewnętrzną metodą";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Visible = false;
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage5);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(756, 147);
+            this.tabControl1.Size = new System.Drawing.Size(758, 172);
             this.tabControl1.TabIndex = 7;
             this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button5);
             this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Controls.Add(this.button4);
             this.tabPage1.Controls.Add(this.button1);
@@ -138,11 +153,20 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(748, 121);
+            this.tabPage1.Size = new System.Drawing.Size(750, 146);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Ustawienia";
             this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(53, 58);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(537, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // tabPage2
             // 
@@ -155,6 +179,25 @@
             this.tabPage2.Text = "Log niezgodności";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.listBox2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(748, 121);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Log błędów";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(0, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(748, 121);
+            this.listBox2.TabIndex = 6;
+            // 
             // tabPage3
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
@@ -163,15 +206,34 @@
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Statystyki";
             this.tabPage3.UseVisualStyleBackColor = true;
-            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.listBox3);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(748, 121);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Log przetworzonych plików";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(0, 0);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(748, 121);
+            this.listBox3.TabIndex = 7;
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 125);
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 150);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(756, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(758, 22);
             this.statusStrip1.TabIndex = 8;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -185,11 +247,26 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Step = 1;
+            this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(756, 147);
+            this.ClientSize = new System.Drawing.Size(758, 172);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
@@ -198,6 +275,8 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -223,6 +302,13 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
     }
 }
 
--- VolumeCompare/Form1.cs
+++ VolumeCompare/Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
@@ -12,6 +14,7 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace VolumeCompare
 {
     public partial class Form1 : Form
@@ -22,10 +25,20 @@ namespace VolumeCompare
         StreamReader from = null;
         FileInfo fi1;
         FileInfo fi2;
+
+        public long total, processed;
+        System.Collections.Specialized.NameValueCollection files = new NameValueCollection();
+        System.Collections.Specialized.NameValueCollection directories = new NameValueCollection();
+
+        List<string> log1 = new List<string>();
+        List<string> log2 = new List<string>();
+        List<string> log3 = new List<string>();
+
         public Form1()
         {
 
             InitializeComponent();
+            backgroundWorker1.WorkerSupportsCancellation = true;
             fi1 = new FileInfo(loc + "\\from.cs");
              fi2 = new FileInfo(loc + "\\to.cs");
 
@@ -80,12 +93,7 @@ namespace VolumeCompare
         }
 
 
-        public  bool compare (string f, string t)
-        {
-            return true;
-
-
-        }
+     
         public string Md5Sum(string file)
         {
 
@@ -136,13 +144,6 @@ namespace VolumeCompare
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            DateTime dt1 = DateTime.Now;
-            Process();
-            DateTime dt2 = DateTime.Now;
-            MessageBox.Show((dt2 - dt1).TotalMilliseconds + "");
-        }
 
         private void Process()
         {
@@ -163,15 +164,15 @@ namespace VolumeCompare
                         if (fi11.Exists)
                         {
                             string s2 = Md5SumByProcess(fi11.FullName);//.ToUpper();
-                            if(String.Compare(s1,s2) !=0) 
-                            listBox1.Items.Add( fi1.FullName+ "             !=             " + fi11.FullName);
+                            if (String.Compare(s1, s2) != 0) log1.Add(fi1.FullName + "             !=             " + fi11.FullName);
+                            //listBox1.Items.Add( fi1.FullName+ "             !=             " + fi11.FullName);
                         }
                         else
                         {
-                            listBox1.Items.Add("Cant find:" + fi11.FullName);
+                            //listBox1.Items.Add("Cant find:" + fi11.FullName);
+                            log1.Add("Cant find:" + fi11.FullName);
                         }
                     }
-                    //listBox1.Items.Add(fi.FullName + " : " + Md5SumByProcess(fi.FullName));
                 }
             }
             catch (UnauthorizedAccessException)
@@ -191,41 +192,74 @@ namespace VolumeCompare
 
 
 
-        /////////////////////////////////////////////////////
-        public static long GetDirectorySize(DirectoryInfo di)
+        private void TreeScan(string sDir)
         {
-            return  di.GetFiles("*.*", SearchOption.AllDirectories).Sum(file => file.Length);
+            foreach (string f in Directory.GetFiles(sDir))
+            {
+                FileInfo fi = new FileInfo(f);
+                total += fi.Length;
+                files.Add(f, fi.Length.ToString());
+            }
+            foreach (string d in Directory.GetDirectories(sDir))
+            {
+                try
+                {
+                    TreeScan(d);
+                }
+                catch (Exception)
+                {
+                    log2.Add("Cant access dir:   " + d);
+                }
+            }
         }
 
-        public long total, processed;
-        private void Process2()
+        
+
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            timer1.Enabled = true;
-            total = 0; processed = 0;
-            DirectoryInfo di = new DirectoryInfo(textBox1.Text);
-            DirectoryInfo di2 = new DirectoryInfo(textBox2.Text);
-            try
+            
+        }
+        DateTime dt2, dt1;
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dt1 = DateTime.Now;
+            if (backgroundWorker1.IsBusy != true)
             {
-                total = GetDirectorySize(di);
-            }
-            catch (Exception)
-            {
+                backgroundWorker1.RunWorkerAsync();
 
             }
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs ea)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            total = 0; processed = 0;
+
+            TreeScan(textBox1.Text);
+
+            timer1.Enabled = true;
+            DirectoryInfo di = new DirectoryInfo(textBox1.Text);
+            DirectoryInfo di2 = new DirectoryInfo(textBox2.Text);
+
+
             try
             {
-                foreach (var fi in di.GetFileSystemInfos("*.*", SearchOption.AllDirectories))
+                IEnumerator myEnumerator = files.GetEnumerator();
+                foreach (String fi in files.AllKeys)
                 {
-                    string s1 = Md5Sum(fi.FullName);//.ToUpper();
-                    string pth = fi.FullName.Replace(textBox1.Text, " ");
+                    long fi_size = Int64.Parse(files[fi]);
+                    string pth = fi;
+                    string s1 = Md5Sum(pth);
+                    processed += fi_size;
+                    pth = pth.Replace(textBox1.Text, " ");
+                    string pth_short = pth.Replace(textBox1.Text, " ");
                     pth = textBox2.Text + "\\" + pth;
                     DirectoryInfo di11 = new DirectoryInfo(pth);
                     FileInfo fi11 = new FileInfo(pth);
-                    try
-                    {
-                        processed += ((FileInfo)fi).Length;
-                    }
-                    catch (Exception) { }
+                    worker.ReportProgress((int)(((double)((double)((double)processed / (double)total)) * ((double)100))));
+
                     if (di11.Exists) { }
                     else
                     {
@@ -233,54 +267,59 @@ namespace VolumeCompare
                         {
                             string s2 = Md5Sum(fi11.FullName);//.ToUpper();
                             if (String.Compare(s1, s2) != 0)
-                                listBox1.Items.Add(fi1.FullName + "             !=             " + fi11.FullName);
+                                log1.Add(fi1.FullName + "             !=             " + fi11.FullName);
+                            else
+                            {
+                                log3.Add("File:   " + fi11.FullName + "    maches file:" + pth_short);
+                                log3.Add(s1 + "   ====   " + s2);
+                            }
                         }
                         else
                         {
-                            listBox1.Items.Add("Cant find:" + fi11.FullName);
+                            log2.Add("Cant find file:    " + fi11.FullName);
                         }
                     }
-                    //listBox1.Items.Add(fi.FullName + " : " + Md5SumByProcess(fi.FullName));
+
                 }
+                listBox1.Items.AddRange(log1.ToArray());
+                listBox2.Items.AddRange(log2.ToArray());
+
             }
-           // catch (UnauthorizedAccessException)
-           // {
-           //     MessageBox.Show("Nie można uzyskać dostępu do folderu");
-          //  }
-            catch (Exception e )
+            catch (UnauthorizedAccessException)
             {
-                MessageBox.Show("Inny błąd: "+e.Message+"\n\n"+e.Data);
+                MessageBox.Show("Nie można uzyskać dostępu do folderu");
             }
+            catch (Exception e)
+            {
+                MessageBox.Show("Inny błąd: " + e.Message + "\n\n" + e.Data);
+            }
+
+
+
             timer1.Enabled = false;
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            DateTime dt1 = DateTime.Now;
-            Process2();
-            DateTime dt2 = DateTime.Now;
-            MessageBox.Show((dt2 - dt1).TotalMilliseconds+"");
+            toolStripStatusLabel1.Text = (e.ProgressPercentage.ToString() + "%");
+            toolStripProgressBar1.Value = e.ProgressPercentage;
         }
 
-        private void tabPage1_Click(object sender, EventArgs e)
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-
-        }
-
-        private void tabPage3_Click(object sender, EventArgs e)
-        {
-            //MessageBox.Show("aaa3");
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            toolStripStatusLabel1.Text =( (double)( (double)(processed / total)) * ((double)100)).ToString("F",
-                  CultureInfo.InvariantCulture);
+            dt2 = DateTime.Now;
+            MessageBox.Show((dt2 - dt1).TotalMilliseconds + "");
         }
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            MessageBox.Show("aaa3");
+            if (tabControl1.SelectedIndex == 4)
+            {
+                listBox3.Items.Clear();
+                listBox3.Items.AddRange(log3.ToArray());
+
+            }
+            // MessageBox.Show("aaa3");
         }
     }
 }
